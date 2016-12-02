46c260ec22e1b8933d3ee682b6b8bdbdb41f4cf5
TRUST\TrustTheBoy <603049583@qq.com>
2016-12-03 05:48:39 +0800

    no message

--- PHP/serverGlobals.php
+++
@@ -1,9 +0,0 @@
-<pre>
-<?php
-	print_r($_SERVER);
-	$v1=120;
-	print_r($GLOBALS);
-	echo $GLOBALS["v1"]."-------------------12---------------------------";
-	$v2="asjdkshjfdisjnfksnf khjk";
-	print_r($GLOBALS);
-?>
